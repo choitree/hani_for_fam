e62142ad09ca5dd075e6d8ca40996127133cb3a0
Tree <gmldbs1109@naver.com>
2022-03-21 16:38:01 +0900

    Initial commit

---
+++ src/main/java/com/haniwon/HaniwonApplication.java
@@ -0,0 +1,15 @@
+package com.haniwon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class HaniwonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaniwonApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/haniwon/controller/BootstrapController.java
@@ -0,0 +1,16 @@
+package com.haniwon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class BootstrapController {
+
+    @GetMapping("/hello")
+    public String dashboard() {
+        return "index";
+    }
+
+}
