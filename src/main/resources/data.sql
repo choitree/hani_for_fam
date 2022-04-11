992ec751b3750700c0d6f2c0891be006e1e42d85
Tree <gmldbs1109@naver.com>
2022-04-12 00:11:55 +0900

    Patient의 id, name, birthday를 복합키로 변경
    - Income의 column에서는 name 삭제

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -25,14 +25,3 @@ INSERT INTO income(id, amount, date, is_acupuncture, is_cash, is_pay, memo, symp
 VALUES(4, 300000, '2022-04-07', 0, 1, 1, '지인 소개', '다이어트 한약', 4);
 INSERT INTO income(id, amount, date, is_acupuncture, is_cash, is_pay, memo, symptom, patient_id)
 VALUES(5, 1000000, '2022-04-07', 0, 1, 1, '지인 소개', '공진단', 7);
-
--- INSERT INTO income(id, amount, date, is_acupuncture, is_cash, is_pay, memo, name, symptom, patient_id)
--- VALUES(1, 1000, '2022-04-07', 1, 1, 1, '지인 소개', '홍길동', '발목 염좌', 6);
--- INSERT INTO income(id, amount, date, is_acupuncture, is_cash, is_pay, name, symptom, patient_id)
--- VALUES(2, 7000, '2022-04-07', 1, 1, 1, '최희윤', '발목 염좌', 4);
--- INSERT INTO income(id, amount, date, is_acupuncture, is_cash, is_pay, memo, name, symptom, patient_id)
--- VALUES(3, 7000, '2022-04-07', 1, 1, 1, '지인 소개', '어나미너스', '발목 염좌', 5);
--- INSERT INTO income(id, amount, date, is_acupuncture, is_cash, is_pay, memo, name, symptom, patient_id)
--- VALUES(4, 300000, '2022-04-07', 0, 1, 1, '지인 소개', '최희윤', '다이어트 한약', 4);
--- INSERT INTO income(id, amount, date, is_acupuncture, is_cash, is_pay, memo, name, symptom, patient_id)
--- VALUES(5, 1000000, '2022-04-07', 0, 1, 1, '지인 소개', '임꺽정', '공진단', 7);
