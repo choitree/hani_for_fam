79909e2fb95ea0784b6dc0dac2c75b2bde030b06
Tree <gmldbs1109@naver.com>
2022-04-11 19:33:32 +0900

    PatientController logger 추가
    - 요청이 들어올 때 로그 찍히도록 추가

--- src/main/java/com/haniwon/controller/PatientController.java
+++ src/main/java/com/haniwon/controller/PatientController.java
@@ -2,8 +2,11 @@ package com.haniwon.controller;
 
 import com.haniwon.dto.ResponseDTO;
 import com.haniwon.dto.patient.request.PatientRequestDTO;
+import com.haniwon.dto.patient.response.PatientResponseDTO;
 import com.haniwon.repository.PatientRepository;
 import com.haniwon.service.PatientService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -12,33 +15,38 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class PatientController {
 
-    private PatientService patientService;
+
+    Logger logger = LoggerFactory.getLogger(PatientController.class);
+    private final PatientService patientService;
     public PatientController(PatientService patientService) {
         this.patientService = patientService;
     }
 
     @PostMapping
     public ResponseEntity<ResponseDTO> createPatient(@RequestBody PatientRequestDTO patientRequestDTO) {
+        logger.info("환자 생성");
         patientService.createPatient(patientRequestDTO);
         return ResponseEntity.ok(new ResponseDTO("OK"));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<ResponseDTO> updatePatient(@RequestBody PatientRequestDTO patientRequestDTO, @PathVariable Long id) {
+        logger.info("환자 수정");
         patientService.updatePatient(patientRequestDTO, id);
         return ResponseEntity.ok(new ResponseDTO("OK"));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ResponseDTO> deletePatient(@RequestBody PatientRequestDTO patientRequestDTO, @PathVariable Long id) {
+    public ResponseEntity<ResponseDTO> deletePatient(@PathVariable Long id) {
+        logger.info("환자 삭제");
         patientService.deletePatient(id);
         return ResponseEntity.ok(new ResponseDTO("OK"));
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ResponseDTO> showPatient(@RequestBody PatientRequestDTO patientRequestDTO, @PathVariable Long id) {
-        patientService.showPatient(id);
-        return ResponseEntity.ok(new ResponseDTO("OK"));
+    public ResponseEntity<PatientResponseDTO> showPatient(@PathVariable Long id) {
+        logger.info("환자 1명 조회");
+        return ResponseEntity.ok(patientService.showPatient(id));
     }
 
     @GetMapping
