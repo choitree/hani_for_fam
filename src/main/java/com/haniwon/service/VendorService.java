608faeb9206f643770326941367618b8795bce23
Tree <gmldbs1109@naver.com>
2022-04-26 11:25:50 +0900

    거래처 등록 [#17]

---
+++ src/main/java/com/haniwon/service/VendorService.java
@@ -0,0 +1,36 @@
+package com.haniwon.service;
+
+import com.haniwon.domain.Outcome;
+import com.haniwon.domain.Vendor;
+import com.haniwon.dto.vendor.request.VendorRequestDTO;
+import com.haniwon.dto.vendor.response.VendorResponseDTO;
+import com.haniwon.exception.VendorExistException;
+import com.haniwon.repository.outcome.OutcomeRepository;
+import com.haniwon.repository.vendor.VendorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+@Service
+public class VendorService {
+
+    private final VendorRepository vendorRepository;
+    private final OutcomeRepository outcomeRepository;
+
+    public VendorService(VendorRepository vendorRepository, OutcomeRepository outcomeRepository) {
+         this.vendorRepository = vendorRepository;
+         this.outcomeRepository = outcomeRepository;
+    }
+
+    public void createVendor(VendorRequestDTO vendorRequestDTO) {
+        if(vendorRepository.findByCompanyName(vendorRequestDTO.getCompanyName()).isPresent()) {
+            throw new VendorExistException("해당 거래처가 이미 존재합니다.");
+        }
+        Vendor vendor = Vendor.createVendor(vendorRequestDTO);
+        System.out.println("vendor = " + vendor);
+        vendorRepository.save(vendor);
+    }
+
+}
