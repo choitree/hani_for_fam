f632dcc6c1c4653df97e579d277b8aee27676b1f
Tree <gmldbs1109@naver.com>
2022-04-26 16:25:25 +0900

    타임존 설정
    - 빈 생명주기를 이용해서, Timezone 자동 설정

--- src/main/java/com/haniwon/HaniwonApplication.java
+++ src/main/java/com/haniwon/HaniwonApplication.java
@@ -4,8 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class HaniwonApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("현재 시각 : " + LocalDateTime.now());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HaniwonApplication.class, args);
