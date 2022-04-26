9baf4e1227db9afdcf91271c27445451f16a556f
Tree <gmldbs1109@naver.com>
2022-04-26 20:34:51 +0900

    예약 주별 조회 [#39]

--- src/main/java/com/haniwon/repository/booking/BookingRepositoryCustom.java
+++ src/main/java/com/haniwon/repository/booking/BookingRepositoryCustom.java
@@ -2,10 +2,15 @@ package com.haniwon.repository.booking;
 
 import com.haniwon.domain.Booking;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface BookingRepositoryCustom {
 
     public Optional<Booking> findByBookingTime(LocalDateTime bookingTime);
+
+    public List<Booking> findAllBookingByWeek(LocalDate startDate, LocalDate endDate);
+
 }
