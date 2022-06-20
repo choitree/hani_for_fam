b6832e25d5800405d6fb09b426771c02a8f49911
Tree <gmldbs1109@naver.com>
2022-06-20 19:03:41 +0900

    환자 추가하기 [#46]
    - 생일 notnull 체크

--- src/main/java/com/haniwon/dto/patient/request/PatientRequestDTO.java
+++ src/main/java/com/haniwon/dto/patient/request/PatientRequestDTO.java
@@ -3,31 +3,40 @@ package com.haniwon.dto.patient.request;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.haniwon.domain.Income;
 import com.haniwon.domain.Patient;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString
 public class PatientRequestDTO {
-
-    private Integer chartId;
+    @NotNull(message = "차트번호를 입력하세요")
+    private Long chartId;
+    @NotBlank(message = "이름을 입력하세요")
     private String name;
+    @NotBlank(message = "성별을 입력하세요")
     private String sex;
+    @NotBlank(message = "휴대폰 번호를 입력하세요")
     private String phone;
 
-    @JsonFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate firstVisit;
 
-    @JsonFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate lastVisit;
 
-    @JsonFormat(pattern = "yyyy-MM-dd")
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotNull(message = "생일을 입력하세요")
     private LocalDate birthday;
 
     private String memo;
